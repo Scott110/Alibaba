7ff0520eb0a49155df32e925fb999349ae38e903
scott_he <heshtjs@163.com>
2016-08-16 16:48:22 +0800

    delete local

--- jenkins-build.sh
+++ jenkins-build.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
+#运行的shell脚本
 
 ./gradlew clean
 
 ./gradlew assembleRelease
+
+fir publish ./app/build/outputs/apk/app-release.apk -T e0d51b156f441ee1c8f6860179cc8a28 -c $(cat changelogs/changelog.txt)
